56cfcbc3198b48de02b8d1b455e2ad9cb851e772
sinbum <sinbum@kakao.com>
2021-10-14 11:13:40 +0900

    "h2 데이터 베이스의 등록과 데이터가 등록이 잘되는지 스프링부트에 내장된 junit5(jupitor의 실행 테스트)"

---
+++ src/main/java/com/mongta/myspringboot/repository/AccountRepository.java
@@ -0,0 +1,13 @@
+package com.mongta.myspringboot.repository;
+
+import java.util.Optional;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import com.mongta.myspringboot.entity.Account;
+
+public interface AccountRepository extends JpaRepository<Account, Long>{
+	Optional<Account> findByUsername(String username);
+	
+	
+}
---
+++ src/test/java/com/mongta/myspringboot/repository/AccountRepositoryTest.java
@@ -0,0 +1,36 @@
+package com.mongta.myspringboot.repository;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+import java.util.Optional;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+
+import com.mongta.myspringboot.entity.Account;
+
+@SpringBootTest
+public class AccountRepositoryTest {
+	
+	@Autowired
+	AccountRepository accountRepository;
+	
+	@Test
+	public void account() throws Exception{
+		
+		Account account = new Account();
+		account.setUsername("mongta");
+		account.setPassword("ab1234");
+		
+		//DB에 object를 저장함 - insert
+		
+		Account savedAcct = accountRepository.save(account);
+		System.out.println("ID"+ savedAcct.getId() + " Name " +savedAcct.getUsername() + "password" + savedAcct.getPassword());
+		assertThat(savedAcct).isNotNull();
+		
+		Optional<Account> optional = accountRepository.findByUsername("mongta");
+		System.out.println(optional.isPresent());
+		
+	}
+}
