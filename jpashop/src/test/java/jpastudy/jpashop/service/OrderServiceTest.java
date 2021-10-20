0aca21f68581d06e880cfd0a33934ecdf3b0c118
sinbum <sinbum@kakao.com>
2021-10-20 13:46:00 +0900

    주문시 생성 테스트방식.

--- jpashop/src/test/java/jpastudy/jpashop/service/OrderServiceTest.java
+++ jpashop/src/test/java/jpastudy/jpashop/service/OrderServiceTest.java
@@ -2,7 +2,10 @@ package jpastudy.jpashop.service;
 
 import jpastudy.jpashop.domain.Address;
 import jpastudy.jpashop.domain.Member;
+import jpastudy.jpashop.domain.Order;
+import jpastudy.jpashop.domain.OrderStatus;
 import jpastudy.jpashop.domain.item.Book;
+import jpastudy.jpashop.domain.item.Item;
 import jpastudy.jpashop.repository.OrderRepository;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -11,9 +14,12 @@ import org.springframework.transaction.annotation.Transactional;
 
 import javax.persistence.EntityManager;
 
+import static org.springframework.test.util.AssertionErrors.assertEquals;
+
 @SpringBootTest
 @Transactional
 class OrderServiceTest {
+
     @Autowired
     EntityManager em;
 
@@ -24,7 +30,25 @@ class OrderServiceTest {
     OrderRepository orderRepository;
 
     @Test
-    public void 상품주문() throws Exception{
+    public void 상품주문() throws Exception {
+        Member member = createMember("몽타", new Address("서울", "동작", "00001"));
+        Item item = createBook("부트책", 10000, 10);
+        int orderCnt = 3;
+
+        //When
+        Long orderId = orderService.order(member.getId(), item.getId(),
+                orderCnt);
+        //Then
+        Order getOrder = orderRepository.findOne(orderId);
+        assertEquals("상품 주문시 상태는 ORDER", OrderStatus.ORDER,
+                getOrder.getStatus());
+        assertEquals("주문한 상품 종류 수가 정확해야 한다.", 1,
+                getOrder.getOrderItems().size());
+        assertEquals("주문 가격은 가격 * 수량이다.", 10000 * 3,
+                getOrder.getTotalPrice());
+        assertEquals("주문 수량만큼 재고가 줄어야 한다.", 7, item.getStockQuantity());
+        assertEquals("주문한 상품의 이름이 같아야한다.","부트책",item.getName());
+
 
     }
 
