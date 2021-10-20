e18831af9b97eb6ca436ee0c6f9de329527a05c5
sinbum <sinbum@kakao.com>
2021-10-20 09:15:33 +0900

    사용자 예외 처리 클래스생성.

--- jpashop/src/main/java/jpastudy/jpashop/domain/OrderItem.java
+++ jpashop/src/main/java/jpastudy/jpashop/domain/OrderItem.java
@@ -1,38 +1,83 @@
 package jpastudy.jpashop.domain;
 
 import jpastudy.jpashop.domain.item.Item;
-import lombok.AccessLevel;
-import lombok.Getter;
-import lombok.NoArgsConstructor;
-import lombok.Setter;
-
 import javax.persistence.*;
 
 @Entity
 @Table(name = "order_item")
-@Getter @Setter
-@NoArgsConstructor(access = AccessLevel.PROTECTED)
 public class OrderItem {
-
-    @Id @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
     @Column(name = "order_item_id")
     private Long id;
-
     //Order와의 관계 n:1
     @ManyToOne(fetch = FetchType.LAZY)
-    @JoinColumn(name="order_id")
-    private  Order order;
-
+    @JoinColumn(name = "order_id")
+    private Order order;
     //Item 과의 관계
     @ManyToOne(fetch = FetchType.LAZY)
     @JoinColumn(name = "item_id")
     private Item item;
-
     //주문가격
     private int orderPrice;
-
     //주문수량
     private int count;
 
+    //<editor-fold defaultstate="collapsed" desc="delombok">
+    @SuppressWarnings("all")
+    public Long getId() {
+        return this.id;
+    }
+    //</editor-fold>
 
+    //<editor-fold defaultstate="collapsed" desc="delombok">
+    @SuppressWarnings("all")
+    public Order getOrder() {
+        return this.order;
+    }
+
+    @SuppressWarnings("all")
+    public Item getItem() {
+        return this.item;
+    }
+
+    @SuppressWarnings("all")
+    public int getOrderPrice() {
+        return this.orderPrice;
+    }
+
+    @SuppressWarnings("all")
+    public int getCount() {
+        return this.count;
+    }
+
+    @SuppressWarnings("all")
+    public void setId(final Long id) {
+        this.id = id;
+    }
+
+    @SuppressWarnings("all")
+    public void setOrder(final Order order) {
+        this.order = order;
+    }
+
+    @SuppressWarnings("all")
+    public void setItem(final Item item) {
+        this.item = item;
+    }
+
+    @SuppressWarnings("all")
+    public void setOrderPrice(final int orderPrice) {
+        this.orderPrice = orderPrice;
+    }
+
+    @SuppressWarnings("all")
+    public void setCount(final int count) {
+        this.count = count;
+    }
+
+    @SuppressWarnings("all")
+    protected OrderItem() {
+    }
+    //</editor-fold>
 }
--- jpashop/src/main/java/jpastudy/jpashop/domain/item/Item.java
+++ jpashop/src/main/java/jpastudy/jpashop/domain/item/Item.java
@@ -1,27 +1,61 @@
 package jpastudy.jpashop.domain.item;
 
-import lombok.Getter;
-import lombok.Setter;
-
 import javax.persistence.*;
 
 @Entity
-@Getter @Setter
 @Inheritance(strategy = InheritanceType.SINGLE_TABLE)
 @DiscriminatorColumn(name = "dtype")
 public abstract class Item {
-
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     @Column(name = "item_id")
     private Long id;
-
     //상품이름
     private String name;
-
     //상품가격
     private int price;
-
     //재고수량
     private int stockQuantity;
+
+    //<editor-fold defaultstate="collapsed" desc="delombok">
+    @SuppressWarnings("all")
+    public Long getId() {
+        return this.id;
+    }
+
+    @SuppressWarnings("all")
+    public String getName() {
+        return this.name;
+    }
+
+    @SuppressWarnings("all")
+    public int getPrice() {
+        return this.price;
+    }
+
+    @SuppressWarnings("all")
+    public int getStockQuantity() {
+        return this.stockQuantity;
+    }
+
+    @SuppressWarnings("all")
+    public void setId(final Long id) {
+        this.id = id;
+    }
+
+    @SuppressWarnings("all")
+    public void setName(final String name) {
+        this.name = name;
+    }
+
+    @SuppressWarnings("all")
+    public void setPrice(final int price) {
+        this.price = price;
+    }
+
+    @SuppressWarnings("all")
+    public void setStockQuantity(final int stockQuantity) {
+        this.stockQuantity = stockQuantity;
+    }
+    //</editor-fold>
 }
